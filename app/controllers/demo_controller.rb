b2c2515e2fb71fc36d3c267b7dc33413471f9d91
moose-1 <cmuse@memphis.edu>
2015-11-08 21:03:57 -0600

    Testing Inline Ruby in HTML and Routes

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,19 @@
+class DemoController < ApplicationController
+  layout false
+  
+  def index
+  	render('hello' )
+  end
+
+  def hello
+  	#render('index')
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def google
+  	redirect_to("http://google.com")
+  end
+end
